3e1f6de2ee55d872ea023ea5178c21ffa6d3aba5
yunyeji <dbsdpwl95@neonexsoft.com>
2023-11-16 13:31:40 +0900

    first commit

---
+++ src/main/java/com/neonexsoft/vclstest/VclsTestApplication.java
@@ -0,0 +1,13 @@
+package com.neonexsoft.vclstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VclsTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VclsTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/neonexsoft/vclstest/VclsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neonexsoft.vclstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VclsTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
